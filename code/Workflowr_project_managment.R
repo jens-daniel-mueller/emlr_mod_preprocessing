2799a459a68612db2c18081276087a6f783ef7bd
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-06-09 15:55:01 +0200

    derive air sea disequilibrium

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 # commit regular changes (locally) and rebuild site
 # this takes only changed files into account
-wflow_publish(all = TRUE, message = "re-run with correct NA definition in input files")
+wflow_publish(all = TRUE, message = "derive air sea disequilibrium")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 # you can also run this code with only some of the files. In this case remove the rebuild=TRUE command
