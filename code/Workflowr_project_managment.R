43c042313bc74df02ac16c78c0ef266f8fa8277f
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-10-20 16:29:39 +0200

    rerun with corrected GLODAP qc flags

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 # commit regular changes (locally) and rebuild site
 # this takes only changed files into account
-wflow_publish(all = TRUE, message = "added cstar field extraction from CESM output")
+wflow_publish(all = TRUE, message = "rerun with corrected GLODAP qc flags")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 # you can also run this code with only some of the files. In this case remove the rebuild=TRUE command
