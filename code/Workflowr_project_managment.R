b1c99208154edce5a311cf366f7939a388463ace
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-05-12 18:17:48 +0200

    read data from all models and write individual files

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 # commit regular changes (locally) and rebuild site
 # this takes only changed files into account
-wflow_publish(all = TRUE, message = "read data from all models")
+wflow_publish(all = TRUE, message = "read data from all models and write individual files")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 # you can also run this code with only some of the files. In this case remove the rebuild=TRUE command
