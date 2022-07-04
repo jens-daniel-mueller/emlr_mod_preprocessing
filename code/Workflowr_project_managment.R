5d7c0eaa4567e51894afaddd5cdfd1850950a91f
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-07-04 12:06:06 +0200

    rerun with writing cstar run D fields

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 # commit regular changes (locally) and rebuild site
 # this takes only changed files into account
-wflow_publish(all = TRUE, message = "rerun with writing dissic fields")
+wflow_publish(all = TRUE, message = "rerun with writing cstar run D fields")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 # you can also run this code with only some of the files. In this case remove the rebuild=TRUE command
