d0575c078729c7230b31be9ce98e40acb20020de
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-11-02 18:05:08 +0100

    included calculation of cstar variants

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 # commit regular changes (locally) and rebuild site
 # this takes only changed files into account
-wflow_publish(all = TRUE, message = "extract Cnat from CESM")
+wflow_publish(all = TRUE, message = "included calculation of cstar variants")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 # you can also run this code with only some of the files. In this case remove the rebuild=TRUE command
@@ -13,17 +13,27 @@ wflow_publish(here::here(
   "analysis",
   c(
     "index.Rmd",
+    "GLODAP_subset_A_monthly_CESM.Rmd",
     "Cant_AD_annual_all_models.Rmd",
     "Cant_CB_annual_all_models.Rmd",
     "Climatology_A_2007_all_models.Rmd",
     "co2_atm.Rmd",
     "GLODAP_subset_A_annual_all_models.Rmd",
-    "GLODAP_subset_A_monthly_CESM.Rmd",
     "surface_ocean.Rmd"
   )
 ),
-message = "rerun for all models",
+message = "rerun incl Sea of Japan",
 republish = TRUE)
+
+
+wflow_publish(here::here(
+  "analysis",
+  c(
+    "index.Rmd",
+    "GLODAP_subset_A_annual_all_models.Rmd"
+  )
+),
+message = "rerun subsetting all models")
 
 
 
